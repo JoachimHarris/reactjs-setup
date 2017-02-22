3c7d31a3065da42a3e571f0f2f3f23cac12b3188
Joachim Harris <joachimharristech@gmail.com>
2017-02-22 08:31:39 +0100

    Added p tag with dynamic props

--- src/app/components/Home.js
+++ src/app/components/Home.js
@@ -5,6 +5,7 @@ export class Home extends React.Component {
         return(
             <div>
                 <p>In a new Component!</p>
+                <p>Hi my name is {this.props.name} and my age is {this.props.age}</p>
             </div>
         )
     }
