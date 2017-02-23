ff3f0677d7d591fec6e3455296729f4e10175691
Joachim Harris <joachimharristech@gmail.com>
2017-02-23 20:53:14 +0100

    I cleaned up the app for new design

--- src/app/components/Home.js
+++ src/app/components/Home.js
@@ -1,39 +1,10 @@
 import React from 'react';
 
 export class Home extends React.Component {
-    constructor(props) {
-        super();
-        this.state = {
-            age: props.initialAge,
-            status: 0
-        };
-    }
-
-    onMakeOlder() {
-        this.setState({
-            age: this.state.age + 3
-        });
-    }
-
     render() {
-        var text = "Something!";
         return(
             <div>
-                <p>In a new Component!</p>
-                <p>{ text }</p>
-                <p>Hi my name is {this.props.name} and my age is {this.state.age}</p>
-                <p>{this.state.status}</p>
-                <p>User object => Name: {this.props.user.name}</p>
-                <div>
-                    <h4>Hobbies</h4>
-                    <ul>
-                        {this.props.user.hobbies.map((hobby, i) => <li key={i}>{hobby}</li>)}
-                    </ul>
-                </div>
-                <hr/>
-                {this.props.children}
-                <hr/>
-                <button onClick={this.onMakeOlder.bind(this)} className="btn btn-primary">Make me older!</button>
+                <p>text</p>
             </div>
         )
     }
--- src/app/index.js
+++ src/app/index.js
@@ -6,34 +6,12 @@ import { Home } from './components/Home';
 
 class App extends React.Component {
     render() {
-        var user = {
-            name: "Joachim",
-            hobbies: ["Sports", "Reading"]
-        };
         return(
-            <div className="container">
-                <div className="row">
-                    <div className="col-xs-10 col-xs-offset-1">
-                        <Header/>
-                    </div>
-                </div>
-                <div className="row">
-                    <div className="col-xs-10 col-xs-offset-1">
-                        <Home name={"Joachim"} initialAge={33} user={user}>
-                            <p>This is the children in the Home component</p>
-                        </Home>
-                    </div>
-                </div>
+            <div>
+                <Home />
             </div>
         )
     }
-}
-
-Home.propTypes = {
-    name: React.PropTypes.string,
-    age: React.PropTypes.number,
-    user: React.PropTypes.object,
-    children: React.PropTypes.element.isRequired
 }
 
 render(<App/>, window.document.getElementById('app'));
