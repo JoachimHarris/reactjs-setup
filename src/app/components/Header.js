4edf5d976a04ab38c041bf190a0118899ab9524e
Joachim Harris <joachimharristech@gmail.com>
2017-02-23 22:54:12 +0100

    updated components with styles

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -1,11 +1,22 @@
 import React from 'react';
 
+import header from '../../sass/header.scss';
+
 export class Header extends React.Component {
     render() {
         return(
-            <header>
-                
-            </header>
+            <div className="headerTop">
+                <div className="row container center-block">
+                    <div className="col-md-6">
+                        <div>
+                            <h1 className="headerTop__headline">My name is Jay Harris</h1>
+                            <p>I am a frontend developer and a designer.</p>
+                            <p></p>
+                        </div>
+                    </div>
+                    <div className="col-md-6">.col-md-6</div>
+                </div>
+            </div>
         )
     }
 }
