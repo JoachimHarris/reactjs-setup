36049c0378c305267c04dbd4bf46fe53acebe0aa
Joachim Harris <joachimharristech@gmail.com>
2017-02-25 22:59:20 +0100

    Added content

--- src/app/components/Footer.js
+++ src/app/components/Footer.js
@@ -1,9 +1,32 @@
 import React from 'react';
 
+import footer from '../../sass/footer.scss';
+
 export class Footer extends React.Component {
     render() {
         return(
-            
+            <div className="footer">
+                <div className="row fluid-container text-center">
+                    <div className="col-md-4">
+                        <span className="ion-ios-heart-outline footer__icon" aria-hidden="true"></span>
+                        <p className="footer__text">
+                            Made in Denmark
+                        </p>
+                    </div>
+                    <div className="col-md-4">
+                        <span className="ion-ios-telephone-outline footer__icon" aria-hidden="true"></span>
+                        <p className="footer__text">
+                            Mobile: +(45)42200190
+                        </p>
+                    </div>
+                    <div className="col-md-4">
+                        <span className="ion-ios-email-outline footer__icon" aria-hidden="true"></span>
+                        <p className="footer__text">
+                            joachimharris@gmail.com
+                        </p>
+                    </div>
+                </div>
+            </div>
         )
     }
 }
