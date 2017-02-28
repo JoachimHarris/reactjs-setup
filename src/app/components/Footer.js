d8a6c3e5062ca9e7b8e66a660de69acaab9737ea
Joachim Harris <joachimharristech@gmail.com>
2017-02-28 08:59:09 +0100

    Small updates

--- src/app/components/Footer.js
+++ src/app/components/Footer.js
@@ -6,24 +6,26 @@ export class Footer extends React.Component {
     render() {
         return(
             <div className="footer">
-                <div className="row fluid-container text-center">
-                    <div className="col-md-4">
-                        <span className="ion-ios-heart-outline footer__icon" aria-hidden="true"></span>
-                        <p className="footer__text">
-                            Made in Denmark
-                        </p>
-                    </div>
-                    <div className="col-md-4">
-                        <span className="ion-ios-telephone-outline footer__icon" aria-hidden="true"></span>
-                        <p className="footer__text">
-                            Mobile: +(45)42200190
-                        </p>
-                    </div>
-                    <div className="col-md-4">
-                        <span className="ion-ios-email-outline footer__icon" aria-hidden="true"></span>
-                        <p className="footer__text">
-                            joachimharris@gmail.com
-                        </p>
+                <div className="container text-center">
+                    <div className="row">
+                        <div className="col-md-4">
+                            <span className="ion-ios-heart-outline footer__icon" aria-hidden="true"></span>
+                            <p className="footer__text">
+                                Made in Denmark
+                            </p>
+                        </div>
+                        <div className="col-md-4">
+                            <span className="ion-ios-telephone-outline footer__icon" aria-hidden="true"></span>
+                            <p className="footer__text">
+                                +(45)42200190
+                            </p>
+                        </div>
+                        <div className="col-md-4">
+                            <span className="ion-ios-email-outline footer__icon" aria-hidden="true"></span>
+                            <p className="footer__text">
+                                joachimharris@gmail.com
+                            </p>
+                        </div>
                     </div>
                 </div>
             </div>
--- src/app/components/Mirror.js
+++ src/app/components/Mirror.js
@@ -6,10 +6,41 @@ export class Mirror extends React.Component {
     render() {
         return(
             <div className="mirror">
-                <div className="row container center-block text-center">
-                    <div className="col-md-4">.col-md-4</div>
-                    <div className="col-md-4">.col-md-4</div>
-                    <div className="col-md-4">.col-md-4</div>
+                <div className="container-fluid text-center">
+                    <div className="row">
+                        <h2 className="mirror__headline--top">3 simple cuts</h2>
+                    </div>
+                </div>
+                <div className="container center-block text-center">
+                    <div className="row">
+                        <div className="col-md-4">
+                            <h3 className="mirror__headline">
+                                1. Browse haircuts
+                            </h3>
+                            <span
+                                className="ion-ios-book-outline mirror__icon"
+                                aria-hidden="true">
+                            </span>
+                        </div>
+                        <div className="col-md-4">
+                            <h3 className="mirror__headline">
+                                2. Choose haircut
+                            </h3>
+                            <span
+                                className="ion-ios-checkmark-outline mirror__icon"
+                                aria-hidden="true">
+                            </span>
+                        </div>
+                        <div className="col-md-4">
+                            <h3 className="mirror__headline">
+                                3. Get haircut
+                            </h3>
+                            <span
+                                className="ion-ios-personadd-outline mirror__icon"
+                                aria-hidden="true">
+                            </span>
+                        </div>
+                    </div>
                 </div>
             </div>
         )
